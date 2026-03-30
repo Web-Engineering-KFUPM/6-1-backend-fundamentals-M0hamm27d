e39616706fb450a618e6ddc27704befd603a45b9
M0hamm27d <s202277540@kfupm.edu.sa>
2026-03-30 08:47:58 +0300

    TODO 4: Start the server on port 3000 is done

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -9,13 +9,10 @@ import cors from 'cors';
 // TODO 2: Create the Express app and store it in a variable named app
 const app = express();
 
-// Allow React to access the server
-app.use(cors());
-
 
 
 // TODO 3: Allow React to access the server
-
+app.use(cors());
 
 
 // TODO 5: Create the home route "/"
@@ -31,3 +28,6 @@ app.use(cors());
 
 
 // TODO 4: Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
